d21599c6fc81a0ea4d3fd36234bec3f052b24a0c
Dima <dmitryl0626@gmail.com>
2020-03-07 14:22:42 +0100

    python script pro cteni dat z ultrazvukoeho senzoru

--- python/main.py
+++ python/main.py
@@ -10,6 +10,7 @@ GPIO_TRIGGER = 18
 GPIO_ECHO = 24
 
 #set GPIO direction (IN / OUT)
+GPIO.setwarnings(False)
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
@@ -23,12 +24,12 @@ def distance():
 
     start = time.time()
 
-    while GPIO.input(echoPin)==0:    #Wait for the echo to go high- starting the measurement.
+    while GPIO.input(GPIO_ECHO)==0:    #Wait for the echo to go high- starting the measurement.
         pass
 
     start = time.time()
 
-    while GPIO.input(echoPin)==1:    #Wait for the echo to go low
+    while GPIO.input(GPIO_ECHO)==1:    #Wait for the echo to go low
         pass
 
     stop = time.time()
@@ -38,7 +39,6 @@ def distance():
     # multiply with the sonic speed (34300 cm/s)
     # and divide by 2, because there and back
     distance = (TimeElapsed * 34300) / 2
-
     return distance
 
 if __name__ == '__main__':
@@ -47,7 +47,6 @@ if __name__ == '__main__':
             dist = distance()
             print ("Measured Distance = %.1f cm" % dist)
             time.sleep(1)
-
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
         print("Measurement stopped by User")
